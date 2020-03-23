ca4bdf5145f53030749daa943739b7c36021350a
Barna József <barna.jozsef@uninfopark.hu>
2020-03-23 21:56:56 +0100

    Initial Commit

---
+++ MparkMobilParkolas_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MparkMobilParkolas_IOS
+//
+//  Created by Administrator on 2020. 03. 23..
+//  Copyright © 2020. BJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
