4dedcb0644fc06d0fdd071d8b51d071a77630d81
Barna József <barna.jozsef@uninfopark.hu>
2020-05-20 21:34:31 +0200

    Tesztre átadott verzió

--- MparkMobilParkolas_IOS/Utils/Utils.swift
+++ MparkMobilParkolas_IOS/Utils/Utils.swift
@@ -5,6 +5,7 @@
 //  Created by Administrator on 2020. 03. 24..
 //  Copyright © 2020. BJ. All rights reserved.
 //
+import Foundation
 
 class Utils {
     
@@ -29,12 +30,12 @@ class Utils {
        var convertPhoneNumber: String = "";
        
        if phoneNumberString != "" {
-           let phoneNumberZone = phoneNumberString[phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 2)...phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 3)]
+        let phoneNumberZone = phoneNumberString[phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 2)...phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 3)]
        
-           let phoneNumber123 = phoneNumberString[phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 4)...phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 6)]
+        let phoneNumber123 = phoneNumberString[phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 4)...phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 6)]
        
-           let phoneNumber4567 = phoneNumberString[phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 7)...phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 10)]
-           convertPhoneNumber = ("(06) " + phoneNumberZone + "/" + phoneNumber123 + "-" + phoneNumber4567);
+        let phoneNumber4567 = phoneNumberString[phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 7)...phoneNumberString.index(phoneNumberString.startIndex, offsetBy: 10)]
+        convertPhoneNumber = ("(06) " + "\(phoneNumberZone)" + "/" + "\(phoneNumber123)" + "-" + "\(phoneNumber4567)");
        } else {
            convertPhoneNumber = "06 30/123-4567"
        }
@@ -42,6 +43,20 @@ class Utils {
     }
     
     func parkolasiIdoKiszamolasa(date: String) -> String {
-        return "00:15:12";
+        let calendar = Calendar.current
+        let aktualisDatum = Date()
+        let startDatumSec = calendar.date(from: DateComponents(calendar: calendar, year: 2020, month: 04, day: 22, hour: 23, minute: 30, second: 00))
+        let masodpercKulonbseg = calendar.dateComponents([.second], from: startDatumSec!, to: aktualisDatum)
+        let masodpercek = masodpercKulonbseg.second
+
+        let pOra = masodpercek! / 3600
+        let pPerc = (masodpercek! - (pOra * 3600)) / 60
+        let pSec = (masodpercek! - (pOra * 3600) - (pPerc * 60))
+        
+        return digitConvert(szam: pOra) + ":" + digitConvert(szam: pPerc) + ":" + digitConvert(szam: pSec)
+    }
+    
+    func digitConvert(szam: Int) -> String {
+        return "\(szam)".count == 1 ? "0" + "\(szam)" : "\(szam)"
     }
 }
